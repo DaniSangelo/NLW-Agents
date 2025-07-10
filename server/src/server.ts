be1c2c1828f25749473ed2458cf386c3416c77c6
Daniel Sângelo <danielsangelo@hotmail.com>
2025-07-10 05:28:05 -0300

    feat: add http server

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import { fastify } from 'fastify'
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifyCors } from '@fastify/cors'
+import { env } from './env.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+    origin: 'http://localhost:5173'
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.listen({ port: env.PORT || 3333}).then(() => {
+    console.info(`Server is running on port ${env.PORT}`)
+})
