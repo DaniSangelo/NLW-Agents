360496b30bad0e36eb987ff580fd124e0ca4f4dd
Daniel Sângelo <danielsangelo@hotmail.com>
2025-07-11 05:40:02 -0300

    feat: add questions table
    - add new scripts in package.json file

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -10,7 +10,10 @@ await seed(db, schema).refine(faker => {
             columns: {
                 name: faker.companyName(),
                 description: faker.loremIpsum()
-            }
+            },
+        },
+        questions: {
+            count: 20,
         }
     }
 })
