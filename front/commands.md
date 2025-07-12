5de8cbdffcea332562c05177724cf8d7ff13a1cc
Daniel Sângelo <danielsangelo@hotmail.com>
2025-07-12 10:01:35 -0300

    feat: recording audio

--- front/commands.md
+++ front/commands.md
@@ -6,3 +6,4 @@ shadcn ui
 npm i react-router-dom
 npm i @tanstack/react-query => fazer requisições rest de dentro do react
 npm i @hookform/resolvers
+npm i @types/dom-speech-recognition -D => tipagem para o typescript reconhecer o MediaRecorder
--- front/src/pages/record-room-audio.tsx
+++ front/src/pages/record-room-audio.tsx
@@ -1,10 +1,68 @@
 import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+
+const isRecordingSupported = !!navigator.mediaDevices
+    && typeof navigator.mediaDevices.getUserMedia == 'function'
+    && typeof window.MediaRecorder === 'function';
 
 export function RecordRoomAudio() {
+    const [isRecording, setIsRecording] = useState(false)
+    const recorder = useRef<MediaRecorder | null>(null)
+
+    function stopRecording() {
+        setIsRecording(false)
+
+        if (recorder.current && recorder.current.state !== 'inactive') {
+            recorder.current.stop()
+            // recorder.current = null
+        }
+    }
+
+    async function startRecording() {
+        if (!isRecordingSupported) {
+            alert('Your browser has no support for audio recording')
+            return;
+        }
+
+        setIsRecording(true);
+
+        const audio = await navigator.mediaDevices.getUserMedia({
+            audio: {
+                echoCancellation: true,
+                noiseSuppression: true,
+                sampleRate: 44100
+            }
+        })
+
+        recorder.current = new MediaRecorder(audio, {
+            mimeType: 'audio/webm',
+            audioBitsPerSecond: 64000
+        })
+
+        recorder.current.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+                console.info(event.data)
+            }
+        }
+
+        recorder.current.onstart = () => {
+            console.info('recording started')
+        }
+
+        recorder.current.onstop = () => {
+            console.info('Recording stoped')
+        }
+
+        recorder.current.start()
+    }
+
     return (
         <div className="h-screen flex items-center justify-center gap-3 flex-col">
-            <Button>Record audio</Button>
-            <p>Recording...</p>
+            {isRecording ? (
+                <Button onClick={stopRecording}>Stop recording</Button>
+            ) : (
+                <Button onClick={startRecording}>Record audio</Button>
+            )}
         </div>
     )
 }
